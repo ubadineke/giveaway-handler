0a59498353e1142f0da3fabcf0ed9947439c40a9
ubadineke <ubadinekeprince@gmail.com>
2024-01-06 12:44:30 +0100

    Fixed issues with app.js file

--- app.js
+++ app.js
@@ -3,19 +3,15 @@ const session = require('express-session');
 const passport = require('passport');
 const router = require('./routes/routes.js')
 const authRoutes = require('./routes/authRoutes.js')
-
 const app = express();
-
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' })
 
 const passportConfig = require('./config/oauth')
 app.use(express.json());
 
 app.use(session({
-<<<<<<< HEAD
-    secret: 'sereqsd',
-=======
     secret: process.env.SESSION_SECRET,
->>>>>>> 10e8d954fe55a38734038596b8054edd452ebc35
     resave: false,
     saveUninitialized: false,
   }));
